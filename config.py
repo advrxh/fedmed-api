323a045289a7806f08341c3d1bf7aec0ffba8d9f
Aadil Varsh <aadilvarshofficial@gmail.com>
2023-07-28 16:24:07 +0530

    update config

--- config.py
+++ config.py
@@ -6,7 +6,12 @@ class CONFIG:
 
     if debug:
         mongo_uri = os.getenv("DEBUG_MONGO_URI")
-        host = os.getenv("DEBUG_HOST")
         redis_host = os.getenv("DEBUG_REDIS_HOST")
         redis_port = os.getenv("DEBUG_REDIS_PORT")
         redis_password = os.getenv("DEBUG_REDIS_PASSWORD")
+
+    else:
+        mongo_uri = os.getenv("MONGO_URI")
+        redis_host = os.getenv("REDIS_HOST")
+        redis_port = os.getenv("REDIS_PORT")
+        redis_password = os.getenv("REDIS_PASSWORD")
